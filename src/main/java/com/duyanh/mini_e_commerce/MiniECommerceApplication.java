e585e8d89133864217011b3bc6421a9baf9dcf40
Duy Anh <duyanhdao297@gmail.com>
2026-03-13 01:10:34 +0700

    chore: initialize spring boot mini ecommerce project

---
+++ src/main/java/com/duyanh/mini_e_commerce/MiniECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.duyanh.mini_e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniECommerceApplication.class, args);
+	}
+
+}
