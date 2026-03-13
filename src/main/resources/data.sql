e301af4ac3ceccb6db54362b266573147106a842
Duy Anh <duyanhdao297@gmail.com>
2026-03-13 14:35:27 +0700

    chore: add data for product and product image table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,194 @@
+INSERT INTO product (title, description, price, stock,thumbnail)
+VALUES
+(
+    'Essence Mascara Lash Princess',
+    'The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.',
+    9.99,
+    99,
+    'https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/thumbnail.webp'
+),
+(
+    'Eyeshadow Palette with Mirror',
+    'The Eyeshadow Palette with Mirror offers a versatile range of eyeshadow shades for creating stunning eye looks. With a built-in mirror, it''s convenient for on-the-go makeup application.',
+    19.99,
+    34,
+    'https://cdn.dummyjson.com/product-images/beauty/eyeshadow-palette-with-mirror/thumbnail.webp'
+),
+(
+    'Powder Canister',
+    'The Powder Canister is a finely milled setting powder designed to set makeup and control shine. With a lightweight and translucent formula, it provides a smooth and matte finish.',
+    14.99,
+    89,
+    'https://cdn.dummyjson.com/product-images/beauty/powder-canister/thumbnail.webp'
+),
+(
+    'Red Lipstick',
+    'The Red Lipstick is a classic and bold choice for adding a pop of color to your lips. With a creamy and pigmented formula, it provides a vibrant and long-lasting finish.',
+    12.99,
+    91,
+    'https://cdn.dummyjson.com/product-images/beauty/red-lipstick/thumbnail.webp'
+),
+(
+    'Red Nail Polish',
+    'The Red Nail Polish offers a rich and glossy red hue for vibrant and polished nails. With a quick-drying formula, it provides a salon-quality finish at home.',
+    8.99,
+    79,
+    'https://cdn.dummyjson.com/product-images/beauty/red-nail-polish/thumbnail.webp'
+),
+(
+    'Calvin Klein CK One',
+    'CK One by Calvin Klein is a classic unisex fragrance, known for its fresh and clean scent. It''s a versatile fragrance suitable for everyday wear.',
+    49.99,
+    29,
+    'https://cdn.dummyjson.com/product-images/fragrances/calvin-klein-ck-one/thumbnail.webp'
+),
+(
+    'Chanel Coco Noir Eau De',
+    'Coco Noir by Chanel is an elegant and mysterious fragrance, featuring notes of grapefruit, rose, and sandalwood. Perfect for evening occasions.',
+    129.99,
+    58,
+    'https://cdn.dummyjson.com/product-images/fragrances/chanel-coco-noir-eau-de/thumbnail.webp'
+),
+(
+    'Dior J''adore',
+    'J''adore by Dior is a luxurious and floral fragrance, known for its blend of ylang-ylang, rose, and jasmine. It embodies femininity and sophistication.',
+    89.99,
+    98,
+    'https://cdn.dummyjson.com/product-images/fragrances/dior-j''adore/thumbnail.webp'
+),
+(
+    'Dolce Shine Eau de',
+    'Dolce Shine by Dolce & Gabbana is a vibrant and fruity fragrance, featuring notes of mango, jasmine, and blonde woods. It''s a joyful and youthful scent.',
+    69.99,
+    4,
+    'https://cdn.dummyjson.com/product-images/fragrances/dolce-shine-eau-de/thumbnail.webp'
+),
+(
+    'Gucci Bloom Eau de',
+    'Gucci Bloom by Gucci is a floral and captivating fragrance, with notes of tuberose, jasmine, and Rangoon creeper. It''s a modern and romantic scent.',
+    79.99,
+    91,
+    'https://cdn.dummyjson.com/product-images/fragrances/gucci-bloom-eau-de/thumbnail.webp'
+),
+(
+    'Annibale Colombo Bed',
+    'The Annibale Colombo Bed is a luxurious and elegant bed frame, crafted with high-quality materials for a comfortable and stylish bedroom.',
+    1899.99,
+    88,
+    'https://cdn.dummyjson.com/product-images/furniture/annibale-colombo-bed/thumbnail.webp'
+),
+(
+    'Annibale Colombo Sofa',
+    'The Annibale Colombo Sofa is a sophisticated and comfortable seating option, featuring exquisite design and premium upholstery for your living room.',
+    2499.99,
+    60,
+    'https://cdn.dummyjson.com/product-images/furniture/annibale-colombo-sofa/thumbnail.webp'
+);
+
+
+INSERT INTO product_image (image_url, product_id)
+VALUES
+(
+    'https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp',
+    1
+),
+(
+    'https://cdn.dummyjson.com/product-images/beauty/eyeshadow-palette-with-mirror/1.webp',
+    2
+),
+(
+    'https://cdn.dummyjson.com/product-images/beauty/powder-canister/1.webp',
+    3
+),
+(
+    'https://cdn.dummyjson.com/product-images/beauty/red-lipstick/1.webp',
+    4
+),
+(
+    'https://cdn.dummyjson.com/product-images/beauty/red-nail-polish/1.webp',
+    5
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/calvin-klein-ck-one/1.webp',
+    6
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/calvin-klein-ck-one/2.webp',
+    6
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/calvin-klein-ck-one/3.webp',
+    6
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/chanel-coco-noir-eau-de/1.webp',
+    7
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/chanel-coco-noir-eau-de/2.webp',
+    7
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/chanel-coco-noir-eau-de/3.webp',
+    7
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/dior-j''adore/1.webp',
+    8
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/dior-j''adore/2.webp',
+    8
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/dior-j''adore/3.webp',
+    8
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/dolce-shine-eau-de/1.webp',
+    9
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/dolce-shine-eau-de/2.webp',
+    9
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/dolce-shine-eau-de/3.webp',
+    9
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/gucci-bloom-eau-de/1.webp',
+    10
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/gucci-bloom-eau-de/2.webp',
+    10
+),
+(
+    'https://cdn.dummyjson.com/product-images/fragrances/gucci-bloom-eau-de/3.webp',
+    10
+),
+(
+    'https://cdn.dummyjson.com/product-images/furniture/annibale-colombo-bed/1.webp',
+    11
+),
+(
+    'https://cdn.dummyjson.com/product-images/furniture/annibale-colombo-bed/2.webp',
+    11
+),
+(
+    'https://cdn.dummyjson.com/product-images/furniture/annibale-colombo-bed/3.webp',
+    11
+),
+(
+    'https://cdn.dummyjson.com/product-images/furniture/annibale-colombo-sofa/1.webp',
+    12
+),
+(
+    'https://cdn.dummyjson.com/product-images/furniture/annibale-colombo-sofa/2.webp',
+    12
+),
+(
+    'https://cdn.dummyjson.com/product-images/furniture/annibale-colombo-sofa/3.webp',
+    12
+);
